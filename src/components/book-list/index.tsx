56e81e5b0b6137b9861e607aff5f7f86ca33bdea
adir-avraham <adir.avraham@gmail.com>
2019-09-18 13:55:03 +0300

    sort-books

--- src/components/book-list/index.tsx
+++ src/components/book-list/index.tsx
@@ -6,6 +6,7 @@ export default class BookList extends React.Component<any, any> {
 
     render() {
         const { books } = this.props
+        books.sort(function(a:any, b:any) { return b.year - a.year;});
         return (
             <div>
                 {books.map((book: any) => <Book key={book.title} {...book} /> )}
