6109855c4f4d8d6730c64864fb594beda54a4e32
adir-avraham <adir.avraham@gmail.com>
2019-09-19 13:48:58 +0300

    correct-last-commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,18 @@ import { booksWithImages } from './images/imagesLoader';
 import BookList from './components/book-list';
 
 
-
+const style = { background: "#B4BFCD", width: "25%" ,display: "inline-block", margin: "3%"}
 
 class App extends React.Component<any, any> {
 
   constructor(props: any) {
     super(props)
 
-    this.state = { booksWithImages }
+    this.state = { booksWithImages, style }
   }
 
   render() {
-  
+    const {booksWithImages, style} = this.state
     return (
       <div className="App">
         <Header style={{ color: "#B4BFCD", background: "#160C59", padding: "40px" }} title="Books App" />
@@ -24,8 +24,12 @@ class App extends React.Component<any, any> {
         <Header title="Search" />
         <div> Show Hebrew books
                   <input type="checkbox" onChange={() => {
-            const filteredHebrew = booksWithImages.filter((book: any) => { return book.language.toLowerCase() === "hebrew"})           
-            this.setState({ booksWithImages: filteredHebrew})
+            const filteredHebrew = booksWithImages.filter((book: any) => { return book.language.toLowerCase() === "hebrew"})  
+            const newStyle = { opacity: 0.2, background: "#B4BFCD", width: "25%" ,display: "inline-block", margin: "3%"}     
+                
+            this.setState({ booksWithImages: filteredHebrew, style: newStyle })
+            
+
           }} />
         </div>
         <div>
@@ -38,16 +42,13 @@ class App extends React.Component<any, any> {
 
      
 
-        <BookList books={this.state.booksWithImages} />
+        <BookList books={booksWithImages} style={style} />
 
       </div>
     )
   }
 
 }
-
-
-
 
 
 
--- src/components/book-list/index.tsx
+++ src/components/book-list/index.tsx
@@ -5,11 +5,11 @@ import Book from "../book/index";
 export default class BookList extends React.Component<any, any> {
 
     render() {
-        const { books } = this.props
+        const { books ,style } = this.props
         books.sort(function(a:any, b:any) { return b.year - a.year;});
         return (
             <div>
-                {books.map((book: any) => <Book key={book.title + Math.round(Math.random() * 99999)} {...book} /> )}
+                {books.map((book: any) => <Book key={book.title + Math.round(Math.random() * 99999)} {...book} style={style} /> )}
             </div>
         )
     }
